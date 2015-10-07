065bf6b909aa7fcc3c5370d7305a77b5bddc7323
VovaFender <vladimir.fiction@gmail.com>
2015-10-07 23:54:17 +0300

    median filter added

--- filter_utils.h
+++ filter_utils.h
@@ -1,13 +1,9 @@
 #include "shared_variables.h"
 
-
-#define WIDTH 640
-#define HEIGHT 480
-
 #define KERNEL_SMALL_SIZE 9
 #define KERNEL_EXTENDED_SIZE 25
 
-#define FILTER_COUNT 6
+#define FILTER_COUNT 9
 
 static const int box_filters[FILTER_COUNT][KERNEL_SMALL_SIZE] = {
   {   0,  0,  0,  0, 1,  0,  0,  0,  0  },  //normal
@@ -16,6 +12,17 @@ static const int box_filters[FILTER_COUNT][KERNEL_SMALL_SIZE] = {
   {   0,  0,  1,  0, 0,  0,  1,  0,  0  },  //motion
   {  -1, -1, -1, -1, 8, -1, -1, -1, -1  },  //edge
   {  -1, -1, -1, -1, 9, -1, -1, -1, -1  }  //sharpen
+};
+
+
+static const int sobel_kernels[2][KERNEL_SMALL_SIZE] = {
+  {-1,-2,-1, 0, 0, 0 ,1,2,1},
+  {-1,0,1, -2, 0, 2, -1, 0, 1}
+};
+
+static const int previt_kernels[2][KERNEL_SMALL_SIZE] = {
+  {-1,-1,-1, 0, 0, 0 ,1,1,1},
+  {-1,0,1, -1, 0, 1, -1, 0, 1}
 };
 
 typedef struct {
@@ -30,6 +37,12 @@ typedef struct {
 } Callable;
 
 void box_filter(const Arg *arg,uint8_t * input);
+void sobel_filter(const Arg *arg,uint8_t * input);
+void median_filter(const Arg *arg,uint8_t * input);
+
+void sort_asc(int *in, int length);
+void swap(int *a, int *b);
+
 
 static int basic_kernel_indexes[KERNEL_SMALL_SIZE] = {
  -(WIDTH + 1), -WIDTH, -(WIDTH - 1),
@@ -45,16 +58,23 @@ static const Callable filters[FILTER_COUNT][KERNEL_SMALL_SIZE] = {
     { &box_filter,  {.filter = box_filters[2], .filter_size = 9, .indexes = basic_kernel_indexes  } },
     { &box_filter,  {.filter = box_filters[3], .filter_size = 9, .indexes = basic_kernel_indexes  } },
     { &box_filter,  {.filter = box_filters[4], .filter_size = 9, .indexes = basic_kernel_indexes  } },
-    { &box_filter,  {.filter = box_filters[5], .filter_size = 9, .indexes = basic_kernel_indexes  } }
+    { &box_filter,  {.filter = box_filters[5], .filter_size = 9, .indexes = basic_kernel_indexes  } },
+    { &sobel_filter,  {.filter = &sobel_kernels[0][0], .filter_size = 9, .indexes = basic_kernel_indexes  } },
+    { &sobel_filter,  {.filter = &previt_kernels[0][0], .filter_size = 9, .indexes = basic_kernel_indexes  } },
+    { &median_filter,  {.filter_size = 9, .indexes = basic_kernel_indexes  } }
+
 };
 
 
 
-static const char* filter_names[6] = {
+static const char* filter_names[FILTER_COUNT] = {
   "Normal",
   "Box",
   "Raised",
   "Motion",
   "Edge detection",
-  "Sharpen"
+  "Sharpen",
+  "Sobel",
+  "Previt",
+  "Median"
 };
